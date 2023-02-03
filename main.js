cdd83fc8a358f70429028d19edf9c20a0f87c1b7
yastalab <talabyas@gmail.com>
2023-02-03 19:44:10 -0300

    "modified"

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-// function $(elementoDeHtml) {
-//     return document.querySelector(elementoDeHtml);
-// }
-
-// window.addEventListener("load", () =>{ 
-// const $openButton = document.querySelector("#open-modal")
-// const $containModal = document.querySelector(".contain-modal")
-// const $closeButton = document.querySelector("#close-modal")
-
-//  $openButton.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     $containModal.classList.add("show-modal")
-// })
-// $closeButton.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     $containModal.classList.remove("show-modal")
-// }) 
-
-//  $openButton.onClick = (event) => {
-//     event.preventDefault();
-//     $containModal.classList.add("show-modal");
-// };
-
-// })
 
 function $(elementoDeHtml) {
     return document.querySelector(elementoDeHtml);
